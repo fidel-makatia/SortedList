16d7d210923b911e1ca0ec87809533fd642d94c2
Fidel Makatia <fidel.makatia@autodesk.com>
2021-09-08 16:09:15 +0300

    first commit

--- SortedList/Program.cs
+++ SortedList/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace SortedList
+using System.Collections.Generic;
+
+namespace SortedList__
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SortedList<int, string> myList = new SortedList<int, string>()
+            {
+                {101,"fIDEL" },
+                 {103, "GGG" },
+                {102, "Makatia" },
+               
+                {105,"KUKU" }
+            };
+
+            myList.Add(107, "lilo");
+            
+
+
+            foreach(var item in myList)
+            {
+                Console.WriteLine(item.Key + ", " + item.Value);
+            }
         }
     }
 }
